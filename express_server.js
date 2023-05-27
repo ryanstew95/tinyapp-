71e918d374147b90af78e67760a1c957592d0d4e
Ryan Stewart <ryanstew17@gmail.com>
2023-05-27 12:34:13 -0300

    updated

--- express_server.js
+++ express_server.js
@@ -52,36 +52,51 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// read: index display all urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   console.log(urlDatabase);
   res.render("urls_index", templateVars);
 });
 
+// create: create new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// show: show single url
 app.get("/urls/:id", (req, res) => {
   const key = req.params.id;
   const templateVars = { id: req.params.id, longURL: urlDatabase[key] };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const key = req.params.id;
+  const longURL = urlDatabase[key];
+  res.redirect(longURL);
+});
 
-// Complete the code so that requests to the
-// endpoint "/u/:id" will redirect to its longURL
-// app.get("/u/:id", (req, res) => {
-//   const key = req.params.id;
-//   const longURL = urlDatabase[key];
-//   res.redirect(longURL);
-// });
-
-
+// save: submit create url
 app.post("/urls", (req, res) => {
   const shortUrl = generateRandomString(6);
   urlDatabase[shortUrl] = req.body.longURL;
   res.redirect(`/urls/${shortUrl}`);
+});
+
+// delete; remove a url
+app.post('/urls/:id/delete', (req, res) => {
+  const key = req.params.id;
+  delete urlDatabase[key];
+  res.redirect('/urls');
+});
+
+// edit: show edit url
+app.get("/urls/:id/edit", (req, res) => {
+  const key = req.params.id;
+  const templateVars = { id: req.params.id, longURL: urlDatabase[key] };
+  res.render("urls/edit", templateVars);
+  res.redirect('/urls');
 });
 
 
